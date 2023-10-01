d6d4c22a5f967b989f4ac068e73a8d927a348e76
Angel0982 <20203l001076@utcv.edu.mx>
2023-09-30 21:18:05 -0600

    jenkins118

--- entrypoint.py
+++ entrypoint.py
@@ -1,11 +1,15 @@
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://usuario:contraseña@localhost/nombre_basededatos'  # Cambia esto según tu configuración
 
-import os
+db = SQLAlchemy(app)
+#import os
 
-from app import create_app
+#from app import create_app
 
-settings_module = os.getenv('APP_SETTINGS_MODULE')
-app = create_app(settings_module)
+#settings_module = os.getenv('APP_SETTINGS_MODULE')
+#app = create_app(settings_module)
 
